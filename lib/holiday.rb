cf5aedfe9c2582aeb44996dd3ab952a986732163
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-01-20 12:46:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
     holiday.each do |key, value|
-      binding.pry 
+
   end 
     end 
 end
