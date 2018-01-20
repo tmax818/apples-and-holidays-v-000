29d2e1e59d2a904326bfcfeba652aa50a1c7c69b
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-01-20 13:55:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,11 +72,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  arr = []
   holiday_hash.each do |season, holiday|
   holiday.each do |key, value|
+      arr = []
     if value.include?("BBQ")
-      arr << key  
+      arr << key 
+      
     end 
   end 
     end 
