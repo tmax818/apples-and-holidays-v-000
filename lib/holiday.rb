b26cb9303ae3393d53d761456e757d867dbf315f
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-01-20 11:52:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_hash.store(season, holiday_name)
-  season[holiday_name] = supply_array
   holiday_hash
 end
 
